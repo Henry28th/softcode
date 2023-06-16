8a3dea07328464c6bf4891be52c305c432035439
Henry28th <princehenry77488@gmail.com>
2023-06-16 13:04:17 +0100

    We are intellectuals with great minds

---
+++ script.js
@@ -0,0 +1,13 @@
+// values and varibles
+console.log("Hello World");
+
+
+let firstname = "Henry";
+console.log (firstname);
+
+
+// Variable Naming Conventions
+let last_name = "Madukwu"
+let $function = 50;
+
+console.log(last_name2);
